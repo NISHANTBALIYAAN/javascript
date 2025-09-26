9505d186cdc85992ce808fe4dde3635686c041c8
NISHANT BALIYAN <nishantbaliyannn419@gmail.com>
2025-09-26 10:11:19 +0000

    conversion

---
+++ basics/class2.js
@@ -0,0 +1,21 @@
+//****************************** conversion,opperation*********
+//let name="98nishant"
+//console.log(typeof name)
+// let isLoggedIn=-8;
+// let BooleanisLoggedIn=Boolean(isLoggedIn);
+// console.log(BooleanisLoggedIn);
+//1=>true
+//0=>false
+//""=> false
+//"nishant"=>true
+//****************************operations************************** */
+console.log(1+2)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+"2")
+console.log("1"+2+3)
+console.log("1"+(2+3))
+console.log(1+"2"+3)
+console.log((1+2)+"3")
+console.log("1"+((2+3)*7))
+console.log(1+2+3*7)
